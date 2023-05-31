b4d747641b50b7861af34400c824b70f08953544
Aleksandra Zaforemska <a.zaforemska2@newcastle.ac.uk>
2023-05-31 07:57:15 +0100

    Replace lat/long with NA if direction=M

--- parse_metadata.py
+++ parse_metadata.py
@@ -3,12 +3,11 @@ import pandas as pd
 import PIL.Image
 import PIL.ExifTags
 from tkinter import filedialog
-#import exiftool
 
 #path = './test'
 
 path = filedialog.askdirectory()
-print(path)
+#print(path)
 
 df = pd.DataFrame()
 
@@ -20,40 +19,42 @@ for i in os.listdir(path):
         for k, v in img._getexif().items()
         if k in PIL.ExifTags.TAGS
         }
-    
+
     #Check exif metadata
     #print(exif)
-    
+
     #Check xmp metadata
-    print(img.getxmp())
-    
+    #print(img.getxmp())
+
     #Get GPS metadata
     gps={}
     for k, v in exif['GPSInfo'].items():
         geo_tag = PIL.ExifTags.GPSTAGS.get(k)
         gps[geo_tag]=v
-        
-    if gps == {}:
+    print(i)
+    print(gps)
+
+    if gps['GPSImgDirectionRef']=='M':
         lat = 'NA'
-        lon = 'NA'
+        long = 'NA'
     else:
         #Get Latitude and Longitude
         lat = gps['GPSLatitude']
         long = gps['GPSLongitude']
-    
+
         #Convert to degrees
         lat = float(lat[0]+(lat[1]/60)+(lat[2]/(3600*100)))
         long = float(long[0]+(long[1]/60)+(long[2]/(3600*100)))
-    
+
         #Negative if LatitudeRef:S or LongitudeRef:W
         if gps['GPSLatitudeRef']=='S':
             lat = -lat
         if gps['GPSLongitudeRef']=='W':
             long = -long
-    
+
     print(lat, long)
     coords = {'filename': i,'latitude': lat, 'longitude': long}
-    
+
     df_loc = pd.DataFrame(coords, index=[0])
     df = pd.concat([df, df_loc], ignore_index=True)
 
