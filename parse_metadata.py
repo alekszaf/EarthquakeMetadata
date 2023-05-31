4c2e79c9ff5a9833673d25edeced3d250d16198e
Aleksandra Zaforemska <a.zaforemska2@newcastle.ac.uk>
2023-05-31 10:01:53 +0100

    Extract from XMP

--- parse_metadata.py
+++ parse_metadata.py
@@ -6,60 +6,79 @@ from tkinter import filedialog
 
 #path = './test'
 
-path = filedialog.askdirectory()
-
-df = pd.DataFrame()
-
-for i in os.listdir(path):
-    image_path = os.path.join(path, i)
-    img = PIL.Image.open(image_path)
-    exif = {
-        PIL.ExifTags.TAGS[k]: v
-        for k, v in img._getexif().items()
-        if k in PIL.ExifTags.TAGS
-        }
-
-    # Check exif metadata
-    #print(exif)
-
-    # Check xmp metadata
-    #print(img.getxmp())
+def get_metadata(path):
     
-    # Get date
-    date = img._getexif()[36867]
-    print(date)
+    # Initialize empty dataframe
+    df = pd.DataFrame()
+    
+    for i in os.listdir(path):
+        image_path = os.path.join(path, i)
+        img = PIL.Image.open(image_path)
+        exif = {
+            PIL.ExifTags.TAGS[k]: v
+            for k, v in img._getexif().items()
+            if k in PIL.ExifTags.TAGS
+            }
 
-    # Get GPS metadata
-    gps={}
-    for k, v in exif['GPSInfo'].items():
-        geo_tag = PIL.ExifTags.GPSTAGS.get(k)
-        gps[geo_tag]=v
-    print(i)
-    print(gps)
+        # Check exif metadata
+        #print(exif)
 
-    if gps['GPSImgDirectionRef']=='M':
-        lat = 'NA'
-        long = 'NA'
-    else:
-        # Get Latitude and Longitude
-        lat = gps['GPSLatitude']
-        long = gps['GPSLongitude']
+        # Check xmp metadata
+        #print(img.getxmp())
+        
+        # Get date
+        date = img._getexif()[36867]
+        print(date)
 
-        # Convert to degrees
-        lat = float(lat[0]+(lat[1]/60)+(lat[2]/(3600*100)))
-        long = float(long[0]+(long[1]/60)+(long[2]/(3600*100)))
+        # Get GPS metadata
+        gps={}
+        for k, v in exif['GPSInfo'].items():
+            geo_tag = PIL.ExifTags.GPSTAGS.get(k)
+            gps[geo_tag]=v
+        print(i)
+        print(gps)
 
-        # Negative if LatitudeRef:S or LongitudeRef:W
-        if gps['GPSLatitudeRef']=='S':
-            lat = -lat
-        if gps['GPSLongitudeRef']=='W':
-            long = -long
+        if gps['GPSImgDirectionRef']=='M':
+            lat = 'NA'
+            long = 'NA'
+        else:
+            # Get Latitude and Longitude
+            lat = gps['GPSLatitude']
+            long = gps['GPSLongitude']
 
-    print(lat, long)
-    coords = {'filename': i, 'date': date, 'latitude': lat, 'longitude': long}
+            # Convert to degrees
+            lat = float(lat[0]+(lat[1]/60)+(lat[2]/(3600*100)))
+            long = float(long[0]+(long[1]/60)+(long[2]/(3600*100)))
 
-    df_loc = pd.DataFrame(coords, index=[0])
-    df = pd.concat([df, df_loc], ignore_index=True)
+            # Negative if LatitudeRef:S or LongitudeRef:W
+            if gps['GPSLatitudeRef']=='S':
+                lat = -lat
+            if gps['GPSLongitudeRef']=='W':
+                long = -long
+
+        print(lat, long)
+        
+        # Access the XMP metadata
+        xmp = img.getxmp().items()
+        for k, v in xmp:
+            xmp = v
+        
+        # Access earthquake metadata
+        emeta = xmp['RDF']['Description']['subject']['Bag']['li']
+        print(emeta)
+        
+        
+        # Create final metadata dictionary
+        meta = {'filename': i, 'date': date, 'latitude': lat, 'longitude': long}
+
+        df_loc = pd.DataFrame(meta, index=[0])
+        df = pd.concat([df, df_loc], ignore_index=True)
 
 
-df.to_csv('photo_location_data.csv', index=False)
+    df.to_csv('photo_location_data.csv', index=False)
+
+if __name__=="__main__":
+    print("Select the image directory")
+    path = filedialog.askdirectory()
+    get_metadata(path)
+    print("Done!")
